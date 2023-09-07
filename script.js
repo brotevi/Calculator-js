a4bbd7310f5b76b3a7b49b7336f767820c2f32e0
Tevin <brwntvn@yahoo.com>
2023-09-07 18:37:15 -0500

    updated code

---
+++ script.js
@@ -0,0 +1,18 @@
+let displayValue = "";
+function calculate(){
+    displayValue = eval(displayValue);
+    document.querySelector('#display').value = displayValue
+
+}
+
+function handleConcat(value){
+displayValue+=value;
+document.querySelector('#display').value = displayValue
+
+}
+
+function clearDisplay(){
+    displayValue="";
+    document.querySelector('#display').value = displayValue
+
+}
